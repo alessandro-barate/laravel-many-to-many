650e5313eb926e8869c3b189c96c9348771bfa03
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-26 09:28:18 +0200

    [UPD] Tag seeder done

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TagSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Schema::disableForeignKeyConstraints();
+
+        Tag::truncate();
+
+        $tags = ['HTML'. 'CSS', 'Javascript', 'Vue', 'PHP', 'Laravel', 'React', 'Python', 'Angular', 'Ruby', 'MySQL', 'SQL', 'Bootstrap'];
+
+        foreach ($tags as $tag) {
+
+            $new_tag = new Tag();
+
+            $new_tag->title = $tag;
+            $new_tag->slug = Str::of($tag)->slyg();
+
+            $new_tag->save();
+        }
+
+        Schema::enableForeignKeyConstraints();
     }
 }
