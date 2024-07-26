479b4eec0c2d07c83d6434123083b66180f90dc7
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-26 09:56:57 +0200

    [UPD] Post_tag table created and populated

---
+++ database/seeders/PostTagSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\PostTag;
+use App\Models\Tag;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostTagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for($i = 0; $i < 10; $i++){
+
+            $new_post_tag = new PostTag();
+
+            $new_post_tag->post_id = Post::inRandomOrder()->first()->id;
+            $new_post_tag->tag_id = Tag::inRandomOrder()->first()->id;
+
+            $new_post_tag->save();
+        }
+
+    }
+}
