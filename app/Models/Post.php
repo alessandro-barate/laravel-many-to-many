3aedffd57da2671911fd185275a692cfa22a2f3b
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-08-25 17:31:36 +0200

    [UPD] Cover image url done

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,6 +15,22 @@ class Post extends Model
 
     // Qui i campi che non vogliamo abilitare al mass update
     protected $guarded = ['id', 'cover_image'];
+
+    protected $appends = ['cover_image_url'];
+
+    // protected function coverImage(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn (string $value) => ucfirst($value),
+    //     );
+    // }
+
+    protected function coverImageUrl(): Attribute
+    {
+        return new Attribute(
+            get: fn () => env('APP_FRONTEND_IMG_URL', 'http://localhost') . $this->cover_image,
+        );
+    }
 
     public function type() 
     {
